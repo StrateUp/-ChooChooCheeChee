e3ff2fa46826bcfbc3e7f7a9f7dae4418615b03d
Kayla Astl <kaylaastl@Kaylas-MacBook-Pro.local>
2017-03-18 08:43:11 -0700

    train

--- logic.js
+++ logic.js
@@ -11,34 +11,42 @@
   // Create a variable to reference the database
  var database = firebase.database();
  	// At the initial load, get a snapshot of the current data.
-  	database.ref().on("value", function(snapshot) {
-
-        //button for adding employees
         $("#submit").on("click", function(event){
           event.preventDefault();
 
           //grab user input
-          var trainName = $("#train-name").val().trim();
-          var trainDest = $("#destination").val().trim();
-          var trainStart = $("#start-time").val().trim();
-          var trainFreq =  $("#frequency").val().trim();    
+          var train_Name = $("#train-name").val().trim();
+          var train_Dest = $("#destination").val().trim();
+          var train_Start = $("#start-time").val().trim();
+          var train_Freq =  $("#frequency").val().trim();    
 
-          console.log(trainName);
-          console.log(trainStart)
+          database.ref().push({
+            trainName: train_Name,
+            trainDest: train_Dest,
+            trainStart: train_Start,
+            trainFreq: train_Freq
+          });
+        
+          
 
         //send the got data into the chart
-        var newRow = $("<tr>")
-            .append( $("<td>").html(trainName))
-            .append( $("<td>").html(trainDest))
-            .append( $("<td>").html(trainFreq))
+
+          });
+
+  	database.ref().on("value", function(snapshot) {
+
+            var newRow = $("<tr>")
+            .append( $("<td>").html(snapshot.val().trainName))
+            .append( $("<td>").html(snapshot.val().trainDest))
+            .append( $("<td>").html(snapshot.val().trainFreq))
             .append( $("<td>"))
             .append( $("<td>"))
             .append( $("<td>"));
 
 
           $(".table").append(newRow);
+//button for adding employees
 
-          });
   });
 
        
